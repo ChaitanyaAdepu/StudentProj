d03f28595b36bc6bb4bb7cd1e718fb8f15594788
Chaitanya A <chaitanyaonly@gmail.com>
2021-10-16 21:42:17 +0530

    added logger

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,10 @@ import com.student.bean.Student;
 @Service
 @Component
 public class StudentService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
 	public StudentService() {
 		
-	}
+	} 
 	public List<Student> getStudents() {
 		return List.of(
 				new Student(
