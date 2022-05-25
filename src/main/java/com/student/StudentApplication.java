035fc67112442957261fc08c22a5a7bac920e4bc
Chaitanya A <chaitanyaonly@gmail.com>
2022-05-25 21:22:13 +0530

    cors issue fix

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -15,6 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.student.bean.Student;
 
@@ -26,5 +28,15 @@ public class StudentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
-	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("*")
+	                    .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+	        }
+	    };
+	}
 }
